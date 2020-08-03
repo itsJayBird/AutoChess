75ead4f76ca5d83e7c21f230aab67773ade34379
Jesse Ochoa <jesse@resoundnetworks.com>
2020-08-03 10:46:50 -0500

    setting up

---
+++ AutoChess/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoChess
+{
+    class DataManager
+    {
+        private string dir;
+        public DataManager()
+        {
+            dir = Environment.CurrentDirectory;
+        }
+        public string[] readHeroesList()
+        {
+            string[] heroesList = System.IO.File.ReadAllLines(dir + "\\heroes.txt");
+            return heroesList;
+        }
+    }
+}
